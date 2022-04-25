91f52d70648727f1c7ca27ed05572f69141feb9f
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-04-25 15:56:01 -0400

    modified DisplayUsers

--- client/src/Pages/DisplayUsers.jsx
+++ client/src/Pages/DisplayUsers.jsx
@@ -6,12 +6,11 @@ const URL = 'http://localhost:3001'
 // const URL = 'https://movie-database-backend.herokuapp.com'
 
 function DisplayUsers() {
-
     useEffect(() => {
         const fetchedUsers = async () => {
             const response = await fetch(`${URL}/users/get-all-users`)
             const fetchedUsers = await response.json()
-            console.log(fetchedUsers)
+            console.log("fetchedUsers ", fetchedUsers)
             return fetchedUsers
         }
         fetchedUsers()

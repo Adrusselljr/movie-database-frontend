8b8ab938d7aca60e18aee31a71c357afdb15612c
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-04-27 14:08:13 -0400

    MODIFIED DisplayUsers/Registration

--- client/src/Pages/DisplayUsers.jsx
+++ client/src/Pages/DisplayUsers.jsx
@@ -1,17 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// const URL = 'http://localhost:3001'
 const URL = 'https://movie-database-backend.herokuapp.com'
 
 function DisplayUsers() {
+    const [users, setUsers] = useState([])
     useEffect(() => {
         const fetchedUsers = async () => {
             const response = await fetch(`${URL}/users/get-all-users`, {
                 mode: "cors"
             })
             const fetchedUsers = await response.json()
+            setUsers(fetchedUsers.payload)
             console.log("fetchedUsers ", fetchedUsers)
             return fetchedUsers
         }
@@ -22,6 +23,10 @@ function DisplayUsers() {
         <div className='App'>
 
             <h1>Hello World From DisplayUsers</h1>
+            
+            {users.map(user => {
+                return <p key={ user._id }>{ user.firstName } { user.lastName }</p>
+            })}
         
         </div>
     )
--- client/src/Pages/Registration.jsx
+++ client/src/Pages/Registration.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import DisplayUsers from './DisplayUsers'
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+const URL = 'https://movie-database-backend.herokuapp.com'
+
 function Registration() {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const navigate = useNavigate()
+
+    const handleSubmit = async () => {
+        const newBody = {
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            password: password
+        }
+
+        const fetchedData = await fetch(`${URL}/users/create-user`, {
+            method: "POST",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newBody)
+        })
+        const parsedData = await fetchedData.json()
+        navigate("/login")
+        return parsedData
+    }
+
     return (
         <div className='App'>
 
@@ -13,39 +43,35 @@ function Registration() {
             <div className="registrationForm">
                 <div className="form-group">
                     <label>First Name:</label>
-                    <input className='form-control' type="text"/>
+                    <input onChange={ e => setFirstName(e.target.value) } value={ firstName } className='form-control' type="text"/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Last Name:</label>
-                    <input className='form-control' type="text"/>
+                    <input onChange={ e => setLastName(e.target.value) } value={ lastName } className='form-control' type="text"/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Username:</label>
-                    <input className='form-control' type="text"/>
+                    <input onChange={ e => setUsername(e.target.value) } value={ username } className='form-control' type="text"/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Email:</label>
-                    <input className='form-control' type="email"/>
+                    <input onChange={ e => setEmail(e.target.value) } value={ email } className='form-control' type="email"/>
                 </div><br/>
 
                 <div className="form-group">
                     <label>Password:</label>
-                    <input className='form-control' type="password"/>
+                    <input onChange={ e => setPassword(e.target.value) } value={ password } className='form-control' type="password"/>
                 </div><br/>
 
-                <button className='btn btn-primary'>Register</button>
+                <button onClick={ handleSubmit } className='btn btn-primary'>Register</button>
             </div><br/>
 
             <h5>Already have an account?</h5><br/>
             <Link to='login' className='btn btn-primary'>Login Here</Link>
 
-
-
-            <DisplayUsers />
-        
         </div>
     )
 }
